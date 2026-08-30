5212b94e67d7294883d97cca5e502f387bb2165d
Miceano <pablolabs36@gmail.com>
2026-08-29 20:35:26 -0500

    Multiplayer

--- src/game/player.c
+++ src/game/player.c
@@ -5944,16 +5944,10 @@ struct sndstate *playerSndStart(s32 arg0, s16 sound, struct sndstate **handle, s
 	s32 vol = -1;
 	s32 pan = -1;
 #ifndef PLATFORM_N64
-	const struct player *pl = (playernum >= 0 && playernum < MAX_PLAYERS) ? g_Vars.players[playernum] : NULL;
+	const struct player *pl = g_Vars.players[playernum];
 	if (g_NetMode && playernum != 0 && pl && pl->prop) {
-		// auto attenuate sounds for remote players based on 3D distance and angle to local player
+		// auto attenuate sounds for remote players
 		psGetTheoreticalVolPan(&pl->prop->pos, pl->prop->rooms, sound, &vol, &pan);
-		if (vol <= 0) {
-			if (handle) {
-				*handle = NULL;
-			}
-			return NULL;
-		}
 	}
 #endif
 	return sndStart(arg0, sound, handle, vol, pan, pitch, fxbus, fxmix);
